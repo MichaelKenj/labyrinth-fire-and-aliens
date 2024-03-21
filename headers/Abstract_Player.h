6d69cd01d6dee469cd9ac00c1b744dd812999d14
Michael <michaelkenjetsyan@gmail.com>
2024-03-21 16:37:10 +0400

    Added generate_fires function

--- headers/Abstract_Player.h
+++ headers/Abstract_Player.h
@@ -1,11 +1,43 @@
 #pragma once
 #include <iostream>
+#include <random>
 
 /// <summary>
 /// Can be (0, 0) to (19, 19)
 /// </summary>
 using Coordinate = std::pair<std::size_t, std::size_t>;
 
+/// <summary>
+/// Generates random number from [x,y]
+/// </summary>
+/// <param name="x"></param>
+/// <param name="y"></param>
+/// <returns></returns>
+std::size_t generateRandomNumber(std::size_t x, std::size_t y) {
+	// Seed the random number generator
+	std::random_device rd;
+	std::mt19937 gen(rd());
+
+	// Define the distribution
+	std::uniform_int_distribution<> dis(x, y);
+
+	// Generate and return the random number
+	return dis(gen);
+}
+
+/// <summary>
+/// Generates random coordinate from [coor1,coor2] 
+/// </summary>
+/// <param name="coor1"></param>
+/// <param name="coor2"></param>
+/// <returns></returns>
+Coordinate  generateRandomCoordinate(Coordinate coor1, Coordinate coor2)
+{
+	Coordinate new_coor;
+	new_coor.first = generateRandomNumber(coor1.first, coor2.first);
+	new_coor.second = generateRandomNumber(coor1.second, coor2.second);
+	return new_coor;
+}
 
 class Abstract_Player
 {
