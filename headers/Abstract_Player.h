07e5cb9c88b066aefdb5d9827a9925b8e3f3186b
Ani Mitoyan <animit30@gmail.com>
2024-03-21 15:14:57 +0400

    brought back not so redundant functions

--- headers/Abstract_Player.h
+++ headers/Abstract_Player.h
@@ -9,6 +9,18 @@ using Coordinate = std::pair<std::size_t, std::size_t>;
 
 class Abstract_Player
 {
+private:
+	Coordinate _position;
 public:
 	virtual void move() = 0;
+
+	void setPosition(Coordinate position)
+	{
+		_position = position;
+	}
+
+	Coordinate getPosition()
+	{
+		return _position;
+	}
 };
