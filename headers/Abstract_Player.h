f7a2d1c2fb6fa4cccabb5940e8a441010a1bb912
Ani Mitoyan <animit30@gmail.com>
2024-03-21 18:06:35 +0400

    added random maze generator

--- headers/Abstract_Player.h
+++ headers/Abstract_Player.h
@@ -1,12 +1,22 @@
 #pragma once
 #include <iostream>
-
+#include <random>
 /// <summary>
 /// Can be (0, 0) to (19, 19)
 /// </summary>
 using Coordinate = std::pair<std::size_t, std::size_t>;
 
+std::size_t generateRandomNumber(std::size_t x, std::size_t y) {
+	// Seed the random number generator
+	std::random_device rd;
+	std::mt19937 gen(rd());
 
+	// Define the distribution
+	std::uniform_int_distribution<> dis(x, y);
+
+	// Generate and return the random number
+	return dis(gen);
+}
 class Abstract_Player
 {
 private:
