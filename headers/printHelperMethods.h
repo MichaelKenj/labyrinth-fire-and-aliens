df142fd4d1d899d2feb7a9c72ace52a6eb918612
Mikayel Kenjetsyan <michaelkenjetsyan@gmail.com>
2024-03-25 17:57:31 +0400

    Some changes

--- headers/printHelperMethods.h
+++ headers/printHelperMethods.h
@@ -35,9 +35,9 @@ void printMainMenuFrame()
 	setCursor(0, 0);
 	srand((unsigned)time(NULL));
 	system("cls");
-	goToXY(10, 5); std::cout << " -------------------------------- ";
+	goToXY(10, 5); std::cout << " +------------------------------+ ";
 	goToXY(10, 6); std::cout << " |  Labyrinth: Fire and Aliens  | ";
-	goToXY(10, 7); std::cout << " --------------------------------";
+	goToXY(10, 7); std::cout << " +------------------------------+ ";
 	goToXY(10, 9); std::cout << "1. Play";
 	goToXY(10, 10); std::cout << "2. Quit";
 	goToXY(10, 11); std::cout << "Select option: ";
@@ -48,9 +48,9 @@ void printChooseGameModeFrame()
 	setCursor(0, 0);
 	srand((unsigned)time(NULL));
 	system("cls");
-	goToXY(10, 5); std::cout << " -------------------------------- ";
+	goToXY(10, 5); std::cout << " +------------------------------+ ";
 	goToXY(10, 6); std::cout << " |        Choose game mode      | ";
-	goToXY(10, 7); std::cout << " --------------------------------";
+	goToXY(10, 7); std::cout << " +------------------------------+ ";
 	goToXY(10, 9); std::cout << "1. Fire";
 	goToXY(10, 10); std::cout << "2. Aliens";
 	goToXY(10, 11); std::cout << "3. Back to main menu";
