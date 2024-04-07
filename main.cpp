bcf31d96bd1c657fab7d2b7898a92be0f1cc3e62
Mikayel Kenjetsyan <michaelkenjetsyan@gmail.com>
2024-04-08 03:12:06 +0400

    final without warnings

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
+	HWND console = GetConsoleWindow();
+	RECT ConsoleRect;
+	GetWindowRect(console, &ConsoleRect);
+	MoveWindow(console, ConsoleRect.left, ConsoleRect.top, 900, 900, TRUE);
 	while (true) 
 	{
 		Game game;
